4bc183f183e601aa5872deb45cc8acb96e4526ab
zipik <zipik@3base.co.il>
2014-11-20 12:33:56 +0200

    Update 20141118104954_create_users.rb

--- db/migrate/20141118104954_create_users.rb
+++ db/migrate/20141118104954_create_users.rb
@@ -1,7 +1,17 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+     t.id :string, index :true
+      t.first_name :string
+      t.last_name :string
+      t.address1 :string
+      t.address2 :string
+      t.city :string
+      t.state :string
+      t.country :int
+      t.zip :string
+      t.phone :string
+      t.email :string
       t.timestamps
     end
   end
