81b6e857bcf077868dca0df73a34f565fe43795e
hadassa <hadassas@3base.co.il>
2014-11-24 12:47:45 +0200

    moddle

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,8 +1,6 @@
 class CartController < ApplicationController
 def index
  @rr = User.all
- p '================================'
- p @rr
   @cartsitems = CartItems.all 
   #find(params[1])
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,4 @@
 class Product < ActiveRecord::Base
-<<<<<<< HEAD
 	has_many :cart_items , dependent: :destroy
-=======
-	
->>>>>>> 0eebe892d5f754e52b341c81adc887d0e9d1d26c
+
 end
