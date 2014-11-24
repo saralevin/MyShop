03bfe1653c17b86254595860043b56a1ab8ff593
zipik <zipik@3base.co.il>
2014-11-24 12:40:29 +0200

    cart index

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,9 +1,21 @@
 class CartController < ApplicationController
 def index
- @rr = User.all
- p '================================'
- p @rr
-  @cartsitems = CartItems.all 
-  #find(params[1])
+  @cart =Cart.find_by user_id: 1
+  @cartsitems = CartItem.where("cart_id = ?", @cart.id)
+  @SubTotal =@cartsitems.sum(:price)
+  @ShoppingCharge =0
+  @Total =0
 end
+
+def destroy
+  @cartsitem = CartItem.find(params[:id])
+  @cartsitem.destroy
+  redirect_to :action => "index"
+  # redirect_to index
+  # articles_path איפה הוא מוגדר ???
+end
+def continue
+	redirect_to user
+	end
+# user
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,4 @@
-class CartItems < ActiveRecord::Base
+class CartItem < ActiveRecord::Base
 	 belongs_to :cart	
 	 belongs_to :product
 end
