bab3dcfe1c5c9108b3ed022cb7c194d4806ee5a6
zipik <zipik@3base.co.il>
2014-11-25 11:40:21 +0200

    cart index +user edit

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,29 @@
 class UserController < ApplicationController
+
   def edit
-  	
+  	 @cart = Cart.find(params[:id])
+  	 p"^^^^^^^^^^^^^^^^^^^^^^^^"
+p @cart
+p @cart.user_id
+   @user = User.find(@cart.user_id)
+   p @user
   end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+    	 redirect_to  :action => "end"
+ 	 else
+  	  render 'edit'
+ 	 end
+  end
+
+  def end
+  end
+
+private
+  def user_params
+    params.require(:user).permit( :first_name,:last_name ,:address1 ,:address2 ,:city ,:state ,:country ,:zip ,:phone ,:email )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,11 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-    resources :product 
+    resources :products 
       resources :cart 
         resources :cart_item 
            resources :user 
-
+ get 'usser/end' => 'user#end'
   # Example resource route with options:
   #   resources :products do
   #     member do
