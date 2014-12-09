87caa8a79c3211a130315ffa2894db60835b79ba
nomiw <neomiw@3base.co.il>
2014-12-09 10:53:06 +0200

    migrate added

--- db/migrate/20141118104959_create_carts.rb
+++ db/migrate/20141118104959_create_carts.rb
@@ -1,12 +1,12 @@
 class CreateCarts < ActiveRecord::Migration
   def change
     create_table :carts do |t|
-    	   t.integer:userId 
+    	  t.integer:userId 
       	t.string:productCode
       	t.integer:quantity
       	t.integer:totalSum
       	t.timestamps
-        
+
     end
   end
 end
